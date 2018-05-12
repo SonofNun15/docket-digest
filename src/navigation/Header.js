132b83be5fecf61254935fdd586924bd982aa64f
Josh Graber <josh.graber.85@gmail.com>
2018-05-12 15:53:02 -0400

    Add basic header component

---
+++ src/navigation/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import './App.css';
+
+const Header = () => {
+  return (
+    <header className="app-header">
+      <ul>
+        <li>About</li>
+        <li>Help</li>
+      </ul>
+      <div class="account"></div>
+    </header>
+  );
+};
+
+export default Header;
