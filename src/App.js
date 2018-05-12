b369583cb81dea960576b7329e6968a754e0800c
Josh Graber <josh.graber.85@gmail.com>
2018-05-12 15:58:26 -0400

    Remove redundant header

--- src/App.js
+++ src/App.js
@@ -8,10 +8,6 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
