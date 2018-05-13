4b82b9b5b106ed26dc0b522d666aafa588ef8d19
msgraber <s.graber@sundialproperties.com>
2018-05-13 13:57:28 -0400

    Added initial registration functionality.

--- src/subscription/LoginDialog.js
+++ src/subscription/LoginDialog.js
@@ -5,6 +5,7 @@ import { FormControl } from 'material-ui/Form';
 import { Link } from 'react-router-dom';
 import api from '../services/api';
 import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
 import Dialog, {
   DialogActions,
   DialogContent,
@@ -15,7 +16,8 @@ import Dialog, {
 class LoginDialog extends React.Component {
     state = {
         email : "",
-        password : ""
+        password : "",
+        regVis : false
     };
     handleClose = () => {
       this.props.onClose();
@@ -27,45 +29,61 @@ class LoginDialog extends React.Component {
         this.props.onClose();
     };
 
+    handleToggleRegistration = () => {
+        const { regVis } = this.state;
+        this.setState({regVis:!regVis});
+    };
+
     onChange = (event) => {
         this.setState({[event.target.name]:event.target.value});
+    };
+
+    renderLoginContent() {
+        const { regVis, email, password } = this.state;
+        if (regVis) {
+            return ;
+        }
+        else {
+            return (
+                <DialogContent>
+                    <DialogContentText>
+                        Login using your e-mail address and password.
+                    </DialogContentText>
+                    <FlatButton onClick={this.handleToggleRegistration}><font size="2">Create Account</font></FlatButton>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="email"
+                        name="email"
+                        value={email}
+                        label="Email Address"
+                        type="email"
+                        fullWidth
+                        onChange={this.onChange}
+                    />
+                    <TextField
+                        margin="dense"
+                        id="password"
+                        name="password"
+                        value={password}
+                        label="Password"
+                        type="password"
+                        fullWidth
+                        onChange={this.onChange}
+                    />
+                </DialogContent>
+            )
+        }
     };
     
     render() {
         const { classes, onClose, selectedValue, ...other } = this.props;
-        const { email, password } = this.state;
 
         return (
             <Dialog onClose={this.handleClose} aria-labelledby="login-dialog" open = {true}>
                 <form onSubmit={this.handleSubmit}>
                     <DialogTitle id="login-dialog">User Login</DialogTitle>
-                    <DialogContent>
-                        <DialogContentText>
-                            Login using your e-mail address and password.
-                        </DialogContentText>
-                        <Link to="/Help"><font size="2">Create Account</font></Link>
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            id="email"
-                            name="email"
-                            value={email}
-                            label="Email Address"
-                            type="email"
-                            fullWidth
-                            onChange={this.onChange}
-                        />
-                        <TextField
-                            margin="dense"
-                            id="password"
-                            name="password"
-                            value={password}
-                            label="Password"
-                            type="password"
-                            fullWidth
-                            onChange={this.onChange}
-                        />
-                    </DialogContent>
+                    {this.renderLoginContent()}
                     <DialogActions>
                         <Button onClick={this.handleClose} color="primary">
                             Cancel
