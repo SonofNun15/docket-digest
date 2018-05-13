ce80f6234f76c47f945cbd4677b56d0557770b15
SamGraber <authorsamgraber@gmail.com>
2018-05-13 15:41:59 -0400

    Don't catch errors in the api
    Catching these and logging them causes the promise to now resolve successfully, which results in the app thinking the login/registration was successful. Instead we need to catch this in the login dialog and close without success.

--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,7 @@ import axios from 'axios';
 class Api {
   login(username, password) {
     return axios.post('/api/users/login', {username, password})
-        .then(result => result.data)
-        .catch(err => console.error(err));
+        .then(result => result.data);
   }
   logout() {
     axios.delete('/api/users/login');
@@ -12,14 +11,12 @@ class Api {
 
   register(name, username, password) {
     return axios.post('/api/users', {name, username, password})
-        .then(result => result.data)
-        .catch(err => console.error(err));
+        .then(result => result.data);
   }
 
   getCourts() {
     return axios.get('/api/courts')
-            .then(result => result.data)
-            .catch(err => console.error(err));
+            .then(result => result.data);
   }
 
   subscribe(category, court, docketNumber) {
