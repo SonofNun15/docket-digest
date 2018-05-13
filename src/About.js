b50e2a6c73879e35e69be69b273bdc5f29020514
Josh Graber <josh.graber.85@gmail.com>
2018-05-13 11:34:55 -0400

    Add copy to about and help

--- src/About.js
+++ src/About.js
@@ -6,12 +6,25 @@ import logo from './logo.svg';
 const About = () => {
     return (
         <div className="image-center">
-            <header>
-                <Link to="/">
-                    <img src={logo} width="320" alt="Docket Digest" />
-                </Link>
-            </header>
-            <p className="text">This is the About Page.</p>
+          <header>
+            <Link to="/">
+              <img src={logo} width="320" alt="Docket Digest" />
+            </Link>
+          </header>
+          <div className="text-container narrow">
+            <p className="text">
+              Docket Digest was developed to provide instant open access to public legal material.
+            </p>
+            <p className="text">
+              Docket Digest allows users to subscribe to updates of federal cases in courts that
+              provide public case feeds.  Users no longer need to pay to access the PACER site to
+              confirm whether updates have been filed.
+            </p>
+            <p className="text">
+              Users will receive 3 free subscriptions, and are charged a small fee for subsequent
+              subscriptions.  Real time updates to court filings
+            </p>
+          </div>
         </div>
     )
 }
