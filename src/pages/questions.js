fb6eff5eb021ae411ca6e9cab426f3bb610e13d8
Josh Graber <josh.graber.85@gmail.com>
2018-05-25 08:45:33 -0400

    Move questions out to separate data file

---
+++ src/pages/questions.js
@@ -0,0 +1,58 @@
+const questions = [
+  {
+    question: 'Do I have to be an attorney to use Docket Digest?',
+    answer: 'No! Docket Digest is designed to provide access of public ' +
+            'information to the entire public.',
+  },
+  {
+    question: 'Where do I get a docket number? How do I look up my court?',
+    answer: 'Docket numbers are printed at the top of legal filings.' +
+            'Court and case information can be searched for free on the PACER website.',
+  },
+  {
+    question: 'How do I register for Docket Digest?',
+    answer: 'Enter Court and Docket number on the home page of Docket Digest.' +
+            'The Subscribe button will prompt you to enter an e-mail and create a password.',
+  },
+  {
+    question: 'Are there any limits to Docket Digest usage?',
+    answer: 'Users can subscribe to as many dockets as they wish. ' +
+            'After 3 subscriptions, there is a fee to add additional cases.',
+  },
+  {
+    question: 'Where do I go to view my transactions?',
+    answer: 'After creating an account password, users can login ' +
+            'to see which dockets they have subscribed to, and edit their subscriptions.',
+  },
+  {
+    question: "What if I can't find the federal court for my case?",
+    answer: 'Some federal courts do not provide open access to their court cases via RSS feed. ' +
+            'Please contact your representatives to encourage better access to this public information.',
+  },
+  {
+    question: "What if I'm looking for a local court case?",
+    answer: 'State and regional court documents are not currently available at Docket Digest.',
+  },
+  {
+    question: 'Do I have to pay for this service?',
+    answer: 'After 3 subscriptions, users will be asked to pay for further subscriptions per year.',
+  },
+  {
+    question: 'Why does Docket Digest charge a fee?',
+    answer: 'Docket Digest charges a fee because PACER, the government source of ' +
+            'federal court documents charges to access these files.  ' +
+            'By aggregating access to PACER of updated filings, we can pass limited ' +
+            'free access to our users.',
+  },
+  {
+    question: 'How often am I billed?',
+    answer: 'Users will be billed at the time of access for each additional case. ' +
+            'Subscription expires after one year.',
+  },
+];
+
+export default questions;
+
+/* <h3>What if my docket number is incorrect?</h3>
+<p className="text">
+</p> */
