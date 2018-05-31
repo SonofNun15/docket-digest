b8f1eebd540cac608e33dd81b32fa0fdf292b045
SamGraber <authorsamgraber@gmail.com>
2018-05-31 15:28:14 -0400

    Unit tests for snackbar reducer
    Added tests to enforce that the state changes are correct.

---
+++ src/services/snackbar.reducer.spec.js
@@ -0,0 +1,30 @@
+import { snackbar } from './snackbar.reducer';
+import { openSnackbar, closeSnackbar } from './snackbar.action';
+
+describe('snackbar.reducer', () => {
+  it('should change the state to open with the given message and type', () => {
+    const currentState = { open: false };
+    const action = openSnackbar('message', 'snackbar type');
+
+    const newState = snackbar(currentState, action);
+
+    expect(newState.open).toEqual(true);
+    expect(newState.message).toEqual('message');
+    expect(newState.snackbarType).toEqual('snackbar type');
+  });
+
+  it('should change the state to closed without changing the message', () => {
+    const currentState = { 
+      open: true,
+      message: 'message',
+      snackbarType: 'snackbar type',
+    };
+    const action = closeSnackbar();
+
+    const newState = snackbar(currentState, action);
+
+    expect(newState.open).toEqual(false);
+    expect(newState.message).toEqual('message');
+    expect(newState.snackbarType).toEqual('snackbar type');
+  });
+});
